a00f611fa59759b67b18850e939cc3e5bd4f15b1
ChookiTT <prajda53@osu.cz>
2026-03-02 11:15:28 +0100

    menucontroller

--- src/main/java/com/example/swi_cafe/MenuController.java
+++ src/main/java/com/example/swi_cafe/MenuController.java
@@ -1,13 +1,12 @@
 package com.example.swi_cafe;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 
-
+@CrossOrigin(origins = "*")
 @RestController
     @RequestMapping("/menu")
     public class MenuController {
@@ -26,5 +25,7 @@ import java.util.List;
     public List<Category> getCategories() {
         return categoryRepository.findAll();
     }
+
+
     }
 
--- src/main/java/com/example/swi_cafe/SwiCafeApplication.java
+++ src/main/java/com/example/swi_cafe/SwiCafeApplication.java
@@ -14,11 +14,13 @@ public class SwiCafeApplication {
     @Bean
     public CommandLineRunner runDataTest(MenuRepository repository) {
         return args -> {
-            // This is where you put your code to add to the "Giant Pile"
+            /*
             repository.save(new MenuItem("Chocolate Cake", 30.00));
             repository.save(new MenuItem("Americano", 70.00));
 
             System.out.println("Items have been saved to the database!");
+         */
         };
+
     }
 }
